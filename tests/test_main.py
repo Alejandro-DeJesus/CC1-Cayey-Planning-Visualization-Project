b960f02e4feb0d036ce29931a2bd1973e8d9cab6
Coder2314 <alejandro.dejesus1@upr.edu>
2025-06-17 16:51:08 -0400

    Reset

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from Planning_Visualization.utils import add
-
-class TestUtils(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-
-if __name__ == "__main__":
-    unittest.main()
